9fbd7a874dbe8b81b4e70006e786b3dfe9670011
Eric <real.dassy@gmail.com>
2023-04-29 18:22:04 +0900

    Creating a function with Copy-on-Write

--- FP/extractCalculatesFromAction.swift
+++ FP/extractCalculatesFromAction.swift
@@ -12,26 +12,54 @@ var shoppingCartTotal: Double = 0
 
 // shoppingCart 전역변수를 수정하기 때문에 액션이다.
 func addItemToCart(item: Item) {
-    shoppingCart = addItem(cart: shoppingCart, item: item)
+    shoppingCart = addElement(array: shoppingCart, element: makeItem(name: "Converse", price: 140, quantity: 10))
     calculateCartTotal()
     
 }
 
-// 지역변수에 인자값을 할당해서 지역변수를 수정하고 리턴하고 있다. 전역변수를 읽거나 수정하는게 없기 때문에 전부 명시적 입력, 출력이다. 따라서 계산이다.
+// cart의 구조만 알고 있는 함수..? 흠
 func addItem(cart: [Item], item: Item) -> [Item] {
-    var newCart = cart
-    newCart.append(item)
+    return addElement(array: cart, element: item)
+}
+
+// addItem 함수에서 일반적으로 사용할 수 있는 유틸리티 함수를 추출. 어느 배열에서나 사용할 수 있다.
+func addElement<T>(array: [T], element: T) -> [T] {
+    var newArray = array
+    newArray.append(element)
     
-    return newCart
+    return newArray
+}
+
+func removeItemByName(cart: [Item], name: String) -> [Item] {
+    
+    for i in 0..<cart.count {
+        if cart[i].name == name {
+            return removeItems(array: cart, index: i)
+        }
+    }
+    
+    return cart
+}
+
+func removeItems(array: [Item], index: Int) -> [Item] {
+    var copy = array
+    copy.remove(at: index)
+    
+    return copy
+}
+
+// Item의 구조만 알고 있는 함수
+func makeItem(name: String, price: Double, quantity: Int) -> Item {
+    return Item(name: name, price: price, quantity: quantity)
 }
 
 // shoppingCartTotal 전역변수를 수정하기 때문에 액션이다.
 func calculateCartTotal() {
     shoppingCartTotal = calculateItemTotal(cart: shoppingCart)
-    changeButton()
+    changeButton(cart: shoppingCart)
     addTaxPrice()
 }
-
+    
 // 지역변수 값을 수정하고 리턴하고 있다. 전역변수를 읽거나 수정하는게 없기 때문에 계산이다.
 func calculateItemTotal(cart: [Item]) -> Double {
     var total: Double = 0
@@ -44,8 +72,8 @@ func calculateItemTotal(cart: [Item]) -> Double {
 }
 
 // if나 else에서 @Published 프로퍼티 래퍼를 적용한 Bool 타입의 프로퍼티를 수정할 것이므로, 액션이다.
-func changeButton() {
-    if decideFreeShipping(total: shoppingCartTotal) {
+func changeButton(cart: [Item]) {
+    if decideFreeShipping(cart: cart) {
         // 무료 배송이 가능하다고 적힌 버튼을 활성화하는 bool 값
     } else {
         // 배송비가 부과되는 기본 버튼을 활성화하는 bool 값
@@ -53,8 +81,8 @@ func changeButton() {
 }
 
 // 인자값을 이용한 bool 값을 리턴하고 있다. 계산이다.
-func decideFreeShipping(total: Double) -> Bool {
-    return total >= 20
+func decideFreeShipping(cart: [Item]) -> Bool {
+    return calculateItemTotal(cart: cart) >= 20
 }
 
 // shoppingCartTotal 전역변수를 수정하기 때문에 액션이다.
@@ -73,6 +101,57 @@ func calculateTax(total: Double) -> Double {
 
 struct Item {
     let name: String
-    let price: Double
+    var price: Double
+    var quantity: Int
 }
+
+
+
+//MARK: Copy-On-Write를 적용시켜서 통제 가능한 범위에서 불변성 구현
+
+func objectSet(_ object: [Item], key: String, value: Double) -> [Item] {
+    var copy = object
+    
+    for i in 0..<copy.count {
+        if copy[i].name == key {
+            copy[i].price = value
+        }
+    }
+    
+    return copy
+}
+
+func objectDelete(_ object: [Item], key: String) -> [Item] {
+    var copy = object
+    for i in 0..<copy.count {
+        if copy[i].name == key {
+            copy.remove(at: i)
+        }
+    }
+    return copy
+}
+
+func setPrice(item: [Item], newPrice: Double) -> [Item] {
+    return objectSet(item, key: "T-shirt", value: newPrice)
+}
+
+
+let shopping = [Item(name: "shoes", price: 10, quantity: 3),
+                Item(name: "socks", price: 3, quantity: 20),
+                Item(name: "pants", price: 27, quantity: 5),
+                Item(name: "t-shirt", price: 7, quantity: 10),
+]
+
+
+func setQuantityByName(cart: [Item], name: String, quantity: Int) -> [Item] {
+    var newCart = cart
+    for i in 0..<newCart.count {
+        if newCart[i].name == name {
+            newCart[i].quantity = quantity
+        }
+    }
+    
+    return newCart
+}
+
 
